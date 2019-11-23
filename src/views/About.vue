6c86ca9a66a13b5a95d6a5d1d6fba7030e75a13a
Hu2y <sanghun_@daum.net>
2019-11-23 13:09:55 +0900

    * vue - apollo - graphql

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <apollo-example></apollo-example>
+  </div>
+</template>
+
+<script>
+import ApolloExample from '../components/ApolloExample'
+
+export default {
+  components: {
+    ApolloExample
+  }
+}
+</script>
+
+<style>
+
+</style>
